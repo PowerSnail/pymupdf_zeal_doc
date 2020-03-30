85a27b4f57b15e8fc69389e6dc786bc82d7e4a24
PowerSnail <hj5fb@virginia.edu>
2020-03-30 03:42:59 -0700

    fix

--- make_docset.py
+++ make_docset.py
@@ -46,8 +46,13 @@ def main():
         return
 
     sp.run(["mkdir", "-p", "user_contrib/PyMuPDF"], check=True)
+    
+    # Making 32x32 and 64x64 icons
+    sp.run(["convert", "PyMuPDF.docset/icon.png", "-resize", "16x16^", "-crop", "16x16+0+0", "user_contrib/PyMuPDF/icon.png"], check=True)
+    sp.run(["convert", "PyMuPDF.docset/icon.png", "-resize", "32x32^", "-crop", "32x32+0+0", "user_contrib/PyMuPDF/icon@2x.png"], check=True)
+
+    # Package docset
     sp.run(["tar", "--exclude='.DS_Store'", "-cvzf", "user_contrib/PyMuPDF/PyMuPDF.tgz", "PyMuPDF.docset"], check=True)
-    sp.run(["cp", "PyMuPDF.docset/icon.png", "user_contrib/PyMuPDF"], check=True)
     sp.run(["cp", "../README.md", "user_contrib/PyMuPDF"], check=True)
     sp.run(["touch", "user_contrib/PyMuPDF/docset.json"], check=True)
     with open("user_contrib/PyMuPDF/docset.json", "w") as file:
